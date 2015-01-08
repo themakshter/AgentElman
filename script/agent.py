530b0d2142f5c8910680817c76ad50a6a7a6376d
Mohammad Ali Khan <mohammad_ali93@live.com>
2015-01-08 15:33:36 +0000

    Add scripts

---
+++ script/agent.py
@@ -0,0 +1,75 @@
+import re
+import urllib
+
+text_match = '([\w.&\ -]+)'
+number_match = '(?:<font color=red>)?([-+]?[0-9]*\.?[0-9]*)(?:</font>)?'
+
+exclude_games = []
+
+agent_results = {}
+
+# Download the results from the ECS TAC server
+
+for game in range(613,656):
+    
+    if game not in exclude_games:
+    
+        f = urllib.urlopen("http://tac.ecs.soton.ac.uk:8080/history/"+str(game)+"/")
+        s = f.read()
+        f.close()
+    
+        if s.count('HTTP ERROR')==0:
+            
+            print "Reading game " + str(game)
+                        
+            r = re.findall(r'(<tr><td>'+text_match+'</td><td>'+number_match+'</td><td>'+number_match+'</td><td>'+number_match+'</td>)', s)
+        
+            for tuple in r:
+            
+                agent_name = tuple[1]
+                agent_utility = tuple[2]
+                agent_cost = tuple[3]
+                agent_score = tuple[4]
+         
+                if agent_name not in agent_results:
+            
+                    agent_results[agent_name] = []
+            
+                agent_results[agent_name].append( (game, float(agent_score)) )
+                
+        else:
+            
+            print "Skipping game " + str(game)   
+           
+    else:
+            
+        print "Skipping game " + str(game)
+
+# Calculate the mean score
+
+agent_means = []
+
+for agent,scores in agent_results.items():
+    
+    total = 0
+    number = 0
+    
+    for game,result in scores:
+        total = total + result
+        number = number + 1
+        
+    agent_means.append( ( agent, round(float(total)/float(number),0), number ) )
+    
+agent_means = sorted(agent_means, key=lambda value: value[1], reverse=True)
+    
+print
+print "MEAN SCORES"
+print
+
+count = 1
+
+for agent,scores,number in agent_means:
+    
+    print str(count) + " \t " + agent + " \t " + str(scores) + " \t " + str(number)
+    
+    count = count + 1
