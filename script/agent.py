e2ca97a4e2a4b3fb9cc03e9fd3819da2614f94e5
Mohammad Ali Khan <mohammad_ali93@live.com>
2015-01-08 16:34:02 +0000

    add stats and improve agent.py

--- script/agent.py
+++ script/agent.py
@@ -11,6 +11,10 @@ agent_results = {}
 agent_nams = {"AgentElman"}
 # Download the results from the ECS TAC server
 
+scores = {}
+positions = {}
+differences = {}
+
 for game in range(613,657):
     
     if game not in exclude_games:
@@ -24,18 +28,26 @@ for game in range(613,657):
             #print "Reading game " + str(game)
                         
             r = re.findall(r'(<tr><td>'+text_match+'</td><td>'+number_match+'</td><td>'+number_match+'</td><td>'+number_match+'</td>)', s)
-        
+            i = 0
             for tuple in r:
+                i+=1
                 agentName = 'AgentElman'
+                
                 
                 agent_name = tuple[1]
                 agent_utility = tuple[2]
                 agent_cost = tuple[3]
                 agent_score = tuple[4]
                 
-                if agent_name == agentName: 
-                    print agent_score
+                if i == 1:
+                    topScorer = float(agent_score)
                 
+                if agent_name == agentName: 
+                    difference = topScorer - float(agent_score)
+                    print agent_score + " " + str(i) + " " + str(difference)
+                    
+                    
+                    
                 if agent_name not in agent_results:
             
                     agent_results[agent_name] = []
